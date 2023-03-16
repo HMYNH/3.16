e4033b4f58a11d4eb3a953d1eb52019487bf3ccf
HMYNH <128033921+HMYNH@users.noreply.github.com>
2023-03-16 17:14:05 +0800

    Add files via upload

---
+++ text_3.14.1/text.1.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+//int get_max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//	{
+//		return y;
+//	}
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int max = get_max(a, b);
+//	printf("max = %d\n",max);
+//	max=get_max(100, 200);
+//	printf("max = %d\n", max);
+//		return 0;
+//}
+//void hmy(int* pa, int* pb)
+//{
+//	int tem = 0;
+//	tem = *pa;
+//	*pa = *pb;
+//	*pb = tem;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	printf("a=%d b=%d\n", a, b);
+//	hmy(&a, &b);
+//	printf("a=%d b=%d\n", a, b);
+//	return  0;
+//}
